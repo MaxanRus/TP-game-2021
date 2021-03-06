e3cefe90426cd2e8af2a7519a3cfeb005c0e861b
MaxanRus <timosha1113@yandex.ru>
2021-03-06 12:53:59 +0300

    Добавлено копирование шейдеров при компиляции и несколько шейдеров

---
+++ shaders/shader.fs
@@ -0,0 +1,11 @@
+#version 330 core
+
+out vec4 FragColor;
+
+void main() {
+  FragColor = vec4(0.7f, 0.01f, 0.01f, 1.0f);
+
+  FragColor.r = pow(FragColor.r, 0.45);
+  FragColor.g = pow(FragColor.g, 0.45);
+  FragColor.b = pow(FragColor.b, 0.45);
+}
