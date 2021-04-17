2b9d052c3dcc90d388819e996e9e27f042175cf4
MaxanRus <MaxanRus@users.noreply.github.com>
2021-04-17 23:07:12 +0000

    deploy: 959451dd17269ca13601254fda6ec2b9703d5a31

--- class_cell.js
+++ class_cell.js
@@ -4,7 +4,7 @@ var class_cell =
     [ "Cell", "class_cell.html#a68055b2d118b4d20c30a4871b60817f0", null ],
     [ "~Cell", "class_cell.html#a230b5e7f66fbfe4e5acfda5d95a99c09", null ],
     [ "Add", "class_cell.html#a4ab1e51777a98363d170f41754e204c0", null ],
-    [ "Draw", "class_cell.html#a068da58be93f0b7d2dc6b3396ca8b8cd", null ],
+    [ "Draw", "class_cell.html#a3d4a0b84cec344e492e1d7cb1241e250", null ],
     [ "GetBuilding", "class_cell.html#a4aaa15e8a22c3b31e78fd1667f2b30ae", null ],
     [ "GetSize", "class_cell.html#a85335f0959cd4097c55fa6bbfd2610bd", null ],
     [ "GetSpeed", "class_cell.html#ac21e31812473f2d969ff434b35a81825", null ],
